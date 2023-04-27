c9c7ea2e8f7c407f5be2c6fbd784eca5af28f0a2
Brandon Loi <126843726+BrandonLoi01@users.noreply.github.com>
2023-04-26 21:31:31 -0700

    Added Satellite Data
    All data regarding satellite, alt, lat, long, and time stored in folder named satellite data

---
+++ satelite.py
@@ -0,0 +1,9 @@
+"""
+    Student: Brandon Loi
+    Module: gladysSatellite
+    Description: This Module is to read and assign each data in the
+                 provided json files to a data structure for proccessing.
+"""
+
+#Import Json Files
+import json
